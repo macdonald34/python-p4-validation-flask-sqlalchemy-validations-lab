49d7b96c3fe521218f4facf36d9462da0d9a0fa8
macdonald34 <+>
2024-04-15 12:15:03 +0300

    completed

--- server/models.py
+++ server/models.py
@@ -12,7 +12,20 @@ class Author(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
-
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError('Name field is required.')
+        elif db.session.query(Author).filter(Author.name == name).first():
+            raise ValueError('Name must be unique.')
+        return name
+    
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if len(phone_number) != 10 or not phone_number.isdigit():
+            raise ValueError("Phone number must be 10 digits.")
+        return phone_number
+    
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
@@ -27,8 +40,34 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators  
+    # Add validators 
+    @validates('content')
+    def validate_content(self, key, content):
+        if content and len(content) < 250:
+            raise ValueError('Post content must be at least 250 characters long.')
+        return content
 
-
+    @validates('summary')
+    def validate_summary(self, key, summary):
+      if summary and len(summary) > 250:
+          raise ValueError('Post summary can have a maximum of 250 characters.')
+      return summary
+    
+    @validates('category')
+    def validate_category(self, key, category):
+        allowed_categories = ['Fiction', 'Non-Fiction']
+        if category not in allowed_categories:
+            raise ValueError(f'Post category must be one of {", ".join(allowed_categories)}.')
+        return category
+    
+    @validates('title')
+    def validate_title(self, key, title):
+        clickbait = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(substring in title for substring in clickbait):
+            raise ValueError("No clickbait found")
+        return title
+    
+    
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
+    
